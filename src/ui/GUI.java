b4ab15bd82cb7f5bcd1c53b4c05b18380c597ac3
Gevorah <jhonsebastianijajiortiz@gmail.com>
2020-11-18 17:59:27 -0500

    basic gui

---
+++ src/ui/GUI.java
@@ -0,0 +1,155 @@
+package ui;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import model.Costing;
+
+public class GUI {
+    @FXML
+    private BorderPane mainPane;
+    
+    @FXML
+    private TextField businesstxt;
+
+    @FXML
+    private PasswordField passtxt;
+    
+    @FXML
+    private TextField unitsIItxt;
+
+    @FXML
+    private TextField pMDIItxt;
+
+    @FXML
+    private TextField pMODIItxt;
+
+    @FXML
+    private TextField pCIFIItxt;
+
+    @FXML
+    private TextField pMDIFtxt;
+
+    @FXML
+    private TextField pMODIFtxt;
+
+    @FXML
+    private TextField pCIFIFtxt;
+
+    @FXML
+    private TextField costStartedtxt;
+
+    @FXML
+    private TextField unitsFinishedtxt;
+
+    @FXML
+    private TextField processlbl;
+
+    @FXML
+    private TextField costIItxt;
+
+    @FXML
+    private TextField unitsIFtxt;
+
+    @FXML
+    private TextField costIFtxt;
+
+    @FXML
+    private TextField acpMDtxt;
+
+    @FXML
+    private TextField acpCIFtxt;
+
+    @FXML
+    private TextField acpMODtxt;
+
+    @FXML
+    private Label businesslbl;
+
+    @FXML
+    private TextField unitsStartedtxt;
+    
+    @FXML
+    private TextField transferCosttxt;
+
+    @FXML
+    private TextField cMDtxt;
+
+    @FXML
+    private TextField cMODtxt;
+
+    @FXML
+    private TextField cCIFtxt;
+
+    
+    
+	private Costing costing;
+	public GUI(Costing costing) {
+		this.costing=costing;
+	}
+	@FXML
+	public void loadMainWindow(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-window.fxml"));
+		fxmlLoader.setController(this);
+		Parent setting = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.setCenter(setting);
+	}
+	
+    @FXML
+    void signIn(ActionEvent event) throws IOException {
+    	loadProcessWindow(null);
+    }
+    
+	@FXML
+	public void loadProcessWindow(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("process-window.fxml"));
+		fxmlLoader.setController(this);
+		Parent setting = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.setCenter(setting);
+	}
+	@FXML
+	public void loadPopUp() throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("pp-pop-up.fxml"));
+		loader.setController(this);
+		DialogPane root = loader.load();
+		Dialog<ButtonType> dialog = new Dialog<>();
+		dialog.setDialogPane(root);
+		dialog.setTitle("PP extra info");
+		dialog.show();
+	}
+	
+	@FXML
+    void methodPEPS(ActionEvent event) {
+
+    }
+	
+	@FXML
+    void methodPP(ActionEvent event) {
+
+    }
+	
+	
+	
+    @FXML
+    void loadConcepts(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadInstructions(ActionEvent event) {
+
+    }
+
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,60 +1,36 @@
 package ui;
+
 import java.io.*;
-public class Main {
-	public static BufferedReader br;
-	public static BufferedWriter bw;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.*;
 
-	public static void main(String args[]) throws IOException {
 
-		br = new BufferedReader(new InputStreamReader(System.in));
-		bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-		solve();
+public class Main extends Application {
+	private GUI gui;
+	private Costing costing;
 
-		br.close();
-		bw.close();
+	public Main() {
+		costing = new Costing();
+		gui = new GUI(costing);
 	}
-    
-	public static void solve()throws IOException{
-		bw.write("\n¿Cuál es el nombre de la empresa?");
-		String nombreEmpresa=br.readLine();
-		bw.write("\n¿Qué periodo se va a costear?");
-		String periodoContable=br.readLine();
-		bw.write("\nQué método desea usar para hacer el costeo: (1)PEPS, (2)PP");
-		int choice = Integer.parseInt(br.readLine());
-		bw.write("\nIngrese las unidades del inventario inicial:");
-		double unidInvInic= Double.parseDouble(br.readLine());
-		bw.write("\nIngrese el portentaje de materiales directos en el inventario inicial:");
-		double porcentajeMDInvInic = Double.parseDouble(br.readLine());
-		bw.write("\nIngrese el portentaje de mano de obra directa en el inventario inicial:");
-		double porcentajeMODInvInic = Double.parseDouble(br.readLine());
-		bw.write("\nIngrese el portentaje de costos indirectos de fabricación en el inventario inicial:");
-		double porcentajeCIFInvInic = Double.parseDouble(br.readLine());
-		
-		switch(choice){
-		    case 1:
-		        bw.write("\nIngrese el costo del inventario inicial");
-		        double costoInvInic = Double.parseDouble(br.readLine());
-		        bw.write("\nIngrese los costos agregados:");
-		        bw.write("\nMaterial directo:");
-		        double costoMDPn = Double.parseDouble(br.readLine());
-		        bw.write("\nMano de obra directa:");
-		        double costoMODPn = Double.parseDouble(br.readLine());
-		        bw.write("\nCostos indirectos de fabricación:");
-		        double costoCIFPn = Double.parseDouble(br.readLine());
-		        
-		        break;
-		    case 2:
-		       
-		        break;
-		}
-		bw.write("\nIngrese las unidades del inventario final:");
-		double unidInvFin= Double.parseDouble(br.readLine());
-		bw.write("\nIngrese el portentaje de materiales directos en el inventario final:");
-		double porcentajeMDInvFin = Double.parseDouble(br.readLine());
-		bw.write("\nIngrese el portentaje de mano de obra directa en el inventario final:");
-		double porcentajeMODInvFin = Double.parseDouble(br.readLine());
-		bw.write("\nIngrese el portentaje de costos indirectos de fabricación en el inventario final:");
-		double porcentajeCIFInvFin = Double.parseDouble(br.readLine());
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("main-pane.fxml"));
+		loader.setController(gui);
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("Procces Costing");
+		gui.loadMainWindow(null);
+		stage.show();
 	}
 }
